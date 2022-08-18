758e5d4a4f881ffdc4a12e186457665fb09d3797
atharvakorpe <atharva.korpe@gmail.com>
2022-08-18 20:11:44 +0530

    dropdown

--- javascript/index.js
+++ javascript/index.js
@@ -1,5 +1,9 @@
 
     // Get the modal
+    var view = document.getElementById("name");
+    var body = document.getElementById("body-fg");
+    var menu = document.getElementById("menu");
+    var option = document.getElementById("abc");
     var con = document.getElementById("contact-box");
     
     // Get the button that opens the modal
@@ -12,12 +16,22 @@
     btn.onclick = function() {
       con.style.display = "block";
     }
-    
+    menu.onclick = function() {
+      option.style.display = "block";
+      body.style.display = "block";
+      view.setAttribute("style","height:fit-content");
+      view.setAttribute("style","padding-bottom: 0.35em");
+    }
+    body.onclick = function() {
+      option.style.display = "none";
+      body.style.display = "none";
+      view.setAttribute("style","height:10vh");
+    }
+
     // When the user clicks on <span> (x), close the modal
     span.onclick = function() {
       con.style.display = "none";
     }
-    
     // When the user clicks anywhere outside of the modal, close it
     window.onclick = function(event) {
       if (event.target == con) {
